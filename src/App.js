5ac0da05009ee08ccccbc4afde77f8917de34d88
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2021-06-08 22:28:57 -0400

    card changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import "./App.css";
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./components/DirectoryComponent";
+import { CAMPSITES } from "./shared/campsites";
 
 class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         // imported campsites object data from shared folder
+         campsites: CAMPSITES,
+      };
+   }
    render() {
       return (
          <div className="App">
+            {/* navbar */}
             <Navbar dark color="primary">
                <div className="container">
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
                </div>
             </Navbar>
-            <Directory />
+            {/* campsite state data is passed to directory component  */}
+            <Directory campsites={this.state.campsites} />
          </div>
       );
    }
