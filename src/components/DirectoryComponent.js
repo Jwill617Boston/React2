128086b7bf24a9309eb4ac58d1a1f0c8e59f909e
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2021-06-26 16:05:32 -0400

    Notes 3

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,47 +1,62 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+   Card,
+   CardImg,
+   CardImgOverlay,
+   CardTitle,
+   Breadcrumb,
+   BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-function RenderDirectoryItem({campsite}) {
-    return (
-        <Card>
-            <Link to={`/directory/${campsite.id}`}>
-                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                <CardImgOverlay>
-                    <CardTitle>{campsite.name}</CardTitle>
-                </CardImgOverlay>
-            </Link>
-        </Card>
-    );
+function RenderDirectoryItem({ campsite }) {
+   return (
+      <Card>
+         {/* Create link:ID */}
+         <Link to={`/directory/${campsite.id}`}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+               <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+         </Link>
+      </Card>
+   );
 }
 
+// PROPS Parent = Main = campsites
 function Directory(props) {
+   const directory = props.campsites.map((campsite) => {
+      return (
+         // Array Mapping = Campsites
+         <div key={campsite.id} className="col-md-5 m-1">
+            {/* Functional Comp */}
+            <RenderDirectoryItem campsite={campsite} />
+         </div>
+      );
+   });
 
-    const directory = props.campsites.map(campsite => {
-        return (
-            <div key={campsite.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite={campsite} />
+   return (
+      <div className="container">
+         {/* ROW 1 */}
+         <div className="row">
+            <div className="col">
+               <Breadcrumb>
+                  <BreadcrumbItem>
+                     <Link to="/home">Home</Link>
+                  </BreadcrumbItem>
+                  <BreadcrumbItem active>Directory</BreadcrumbItem>
+               </Breadcrumb>
+               <h2>Directory</h2>
+               <hr />
             </div>
-        );
-    });
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Directory</BreadcrumbItem>
-                    </Breadcrumb>
-                    <h2>Directory</h2>
-                    <hr />
-                </div>
-            </div>
-            <div className="row">
-                {directory}
-            </div>
-        </div>
-    );
+         </div>
+         {/* ROW 2 */}
+         <div className="row">
+            {/* CONST */}
+            {directory}
+         </div>
+      </div>
+   );
 }
 
 export default Directory;
