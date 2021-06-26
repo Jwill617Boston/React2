016500a4b631a03cc66addb038cb971c34e72cea
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2021-06-26 15:56:59 -0400

    Notes 2

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
+// PROPS Parent = Home()
+// item = object array
 function RenderCard({ item }) {
    return (
       <Card>
@@ -13,16 +15,20 @@ function RenderCard({ item }) {
    );
 }
 
+// PROP Parent = Home = campsite, promotion, partner
 function Home(props) {
    return (
       <div className="container">
          <div className="row">
+            {/* Col1 = Campsite */}
             <div className="col-md m-1">
                <RenderCard item={props.campsite} />
             </div>
+            {/* Col2 = promotion  */}
             <div className="col-md m-1">
                <RenderCard item={props.promotion} />
             </div>
+            {/* Col3 = partner */}
             <div className="col-md m-1">
                <RenderCard item={props.partner} />
             </div>
