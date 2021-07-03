8a527d25accdd66fec2840746beffc8f320899e5
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2021-07-03 14:29:37 -0400

    WK4 Complete

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,65 +1,183 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { Component, useState } from "react";
+import {
+   Card,
+   CardImg,
+   CardText,
+   CardBody,
+   Breadcrumb,
+   BreadcrumbItem,
+   Button,
+   Modal,
+   Label,
+   ModalHeader,
+   ModalBody,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { LocalForm, Control, Errors } from "react-redux-form";
 
-function RenderCampsite({campsite}) {
-    return (
-        <div className="col-md-5 m-1">
-            <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>
-        </div>
-    );
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         isModalOpen: false,
+      };
+      this.toggleModal = this.toggleModal.bind(this);
+   }
+
+   toggleModal() {
+      this.setState({
+         isModalOpen: !this.state.isModalOpen,
+      });
+   }
+
+   handleSubmit(values) {
+      console.log("Current state is: " + JSON.stringify(values));
+      alert("Current state is: " + JSON.stringify(values));
+   }
+
+   render() {
+      return (
+         <>
+            <Button outline onClick={this.toggleModal}>
+               <i className="fa fa-pencil fa-lg" /> Submit Comment
+            </Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+               <ModalHeader toggle={this.toggleModal}>
+                  Submit Comment
+               </ModalHeader>
+               <ModalBody>
+                  <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                     {/* Rating */}
+                     <div className="form-group">
+                        <Label htmlFor="rating">Rating</Label>
+                        <Control.select
+                           model=".rating"
+                           id="rating"
+                           name="rating"
+                           className="form-control"
+                        >
+                           <option>1</option>
+                           <option>2</option>
+                           <option>3</option>
+                           <option>4</option>
+                           <option>5</option>
+                        </Control.select>
+                     </div>
+                     {/* Name */}
+                     <div className="form-group">
+                        <Label htmlFor="author">Your Name</Label>
+                        <Control.text
+                           model=".author"
+                           id="author"
+                           name="author"
+                           className="form-control"
+                           validators={{
+                              minLength: minLength(2),
+                              maxLength: maxLength(15),
+                           }}
+                        ></Control.text>
+                        <Errors
+                           className="text-danger"
+                           model=".author"
+                           show="touched"
+                           component="div"
+                           messages={{
+                              minLength: "Must be at least 2 characters",
+                              maxLength: "Must be 15 characters or less",
+                           }}
+                        />
+                     </div>
+                     {/* Comment */}
+                     <div className="form-group">
+                        <Label htmlFor="text">Comment</Label>
+                        <Control.textarea
+                           model=".text"
+                           id="text"
+                           name="text"
+                           className="form-control"
+                        ></Control.textarea>
+                     </div>
+                  </LocalForm>
+                  <Button outline className="fa fa-lg fa-pencil" onClick="">
+                     Submit Comment
+                  </Button>
+               </ModalBody>
+            </Modal>
+         </>
+      );
+   }
 }
 
-function RenderComments({comments}) {
-    if (comments) {
-        return (
-            <div className="col-md-5 m-1">
-                <h4>Comments</h4>
-                {
-                    comments.map(comment => {
-                        return (
-                            <div key={comment.id}>
-                                <p>
-                                    {comment.text}<br />
-                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                </p>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-        );
-    }
-    return <div />;
+function RenderCampsite({ campsite }) {
+   return (
+      <div className="col-md-5 m-1">
+         <Card>
+            <CardImg top src={campsite.image} alt={campsite.name} />
+            <CardBody>
+               <CardText>{campsite.description}</CardText>
+            </CardBody>
+         </Card>
+      </div>
+   );
+}
+
+function RenderComments({ comments }) {
+   if (comments) {
+      return (
+         <div className="col-md-5 m-1">
+            <h4>Comments</h4>
+            {comments.map((comment) => {
+               return (
+                  <div key={comment.id}>
+                     <p>
+                        {comment.text}
+                        <br />
+                        -- {comment.author},{" "}
+                        {new Intl.DateTimeFormat("en-US", {
+                           year: "numeric",
+                           month: "short",
+                           day: "2-digit",
+                        }).format(new Date(Date.parse(comment.date)))}
+                     </p>
+                  </div>
+               );
+            })}
+            <CommentForm />
+         </div>
+      );
+   }
+   return <div />;
 }
 
 function CampsiteInfo(props) {
-    if (props.campsite) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>{props.campsite.name}</h2>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
-                </div>
+   if (props.campsite) {
+      return (
+         <div className="container">
+            <div className="row">
+               <div className="col">
+                  <Breadcrumb>
+                     <BreadcrumbItem>
+                        <Link to="/directory">Directory</Link>
+                     </BreadcrumbItem>
+                     <BreadcrumbItem active>
+                        {props.campsite.name}
+                     </BreadcrumbItem>
+                  </Breadcrumb>
+                  <h2>{props.campsite.name}</h2>
+                  <hr />
+               </div>
             </div>
-        );
-    }
-    return <div />;
+            <div className="row">
+               <RenderCampsite campsite={props.campsite} />
+               <RenderComments comments={props.comments} />
+            </div>
+         </div>
+      );
+   }
+   return <div />;
 }
 
 export default CampsiteInfo;
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,26 +7,44 @@ import {
    Collapse,
    NavItem,
    Jumbotron,
+   Button,
+   Modal,
+   ModalHeader,
+   ModalBody,
+   Form,
+   FormGroup,
+   Input,
+   Label,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
-
 class Header extends Component {
    constructor(props) {
       super(props);
-      // Toggle Data
-      this.toggleNav = this.toggleNav.bind(this);
       this.state = {
          isNavOpen: false,
+         isModalOpen: false,
       };
+      this.toggleNav = this.toggleNav.bind(this);
+      this.toggleModal = this.toggleModal.bind(this);
+      this.handleLogin = this.handleLogin.bind(this);
    }
-
-   // Toggle Function
    toggleNav() {
       this.setState({
          isNavOpen: !this.state.isNavOpen,
       });
    }
-
+   toggleModal() {
+      this.setState({
+         isModalOpen: !this.state.isModalOpen,
+      });
+   }
+   handleLogin(event) {
+      alert(
+         `Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`
+      );
+      this.toggleModal();
+      event.preventDefault();
+   }
    render() {
       return (
          <React.Fragment>
@@ -40,7 +58,6 @@ class Header extends Component {
                   </div>
                </div>
             </Jumbotron>
-
             <Navbar dark sticky="top" expand="md">
                <div className="container">
                   <NavbarBrand className="mr-auto" href="/">
@@ -51,31 +68,24 @@ class Header extends Component {
                         alt="NuCamp Logo"
                      />
                   </NavbarBrand>
-                  {/* Nav Toggled */}
                   <NavbarToggler onClick={this.toggleNav} />
-                  {/* Toggled View */}
                   <Collapse isOpen={this.state.isNavOpen} navbar>
-                     {/* Nav Bar in Toggle */}
                      <Nav navbar>
-                        {/* Home */}
                         <NavItem>
                            <NavLink className="nav-link" to="/home">
                               <i className="fa fa-home fa-lg" /> Home
                            </NavLink>
                         </NavItem>
-                        {/* Directory  */}
                         <NavItem>
                            <NavLink className="nav-link" to="/directory">
                               <i className="fa fa-list fa-lg" /> Directory
                            </NavLink>
                         </NavItem>
-                        {/* About Us */}
                         <NavItem>
                            <NavLink className="nav-link" to="/aboutus">
                               <i className="fa fa-info fa-lg" /> About
                            </NavLink>
                         </NavItem>
-                        {/* Contact Us */}
                         <NavItem>
                            <NavLink className="nav-link" to="/contactus">
                               <i className="fa fa-address-card fa-lg" /> Contact
@@ -83,12 +93,54 @@ class Header extends Component {
                            </NavLink>
                         </NavItem>
                      </Nav>
+                     <span className="navbar-text ml-auto">
+                        <Button outline onClick={this.toggleModal}>
+                           <i className="fa fa-sign-in fa-lg" /> Login
+                        </Button>
+                     </span>
                   </Collapse>
                </div>
             </Navbar>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+               <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+               <ModalBody>
+                  <Form onSubmit={this.handleLogin}>
+                     <FormGroup>
+                        <Label htmlFor="username">Username</Label>
+                        <Input
+                           type="text"
+                           id="username"
+                           name="username"
+                           innerRef={(input) => (this.username = input)}
+                        />
+                     </FormGroup>
+                     <FormGroup>
+                        <Label htmlFor="password">Password</Label>
+                        <Input
+                           type="password"
+                           id="password"
+                           name="password"
+                           innerRef={(input) => (this.password = input)}
+                        />
+                     </FormGroup>
+                     <FormGroup check>
+                        <Label check>
+                           <Input
+                              type="checkbox"
+                              name="remember"
+                              innerRef={(input) => (this.remember = input)}
+                           />
+                           Remember me
+                        </Label>
+                     </FormGroup>
+                     <Button type="submit" value="submit" color="primary">
+                        Login
+                     </Button>
+                  </Form>
+               </ModalBody>
+            </Modal>
          </React.Fragment>
       );
    }
 }
-
 export default Header;
